11236aec4f329ca82ff401b2694a6ff867e3062b
RuffledZest <vibhanshalok@gmail.com>
2024-03-19 14:10:36 +0530

    made basic frontend and default backend

---
+++ src/faucets_frontend/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { faucets_backend } from 'declarations/faucets_backend';
+import Navbar from './components/Navbar';
+
+function App() {
+  const [greeting, setGreeting] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    faucets_backend.greet(name).then((greeting) => {
+      setGreeting(greeting);
+    });
+    return false;
+  }
+
+  return (
+    
+    <>
+      
+      
+      <Navbar/>
+      <br />
+      <br />
+      
+      <form action="#" onSubmit={handleSubmit}>
+        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" />
+        <button type="submit">Click Me!</button>
+      </form>
+      <section id="greeting">{greeting}</section>
+    
+      
+    </>
+  );
+}
+
+export default App;
